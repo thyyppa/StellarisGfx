13bb17072478b431cd11dfa060845c777b1d8168
Travis Hyyppä <travishyyppa@gmail.com>
2017-01-16 14:38:13 -0700

    Update demo

--- app/Demo.cpp
+++ app/Demo.cpp
@@ -1,6 +1,6 @@
 #include <text/lgfont.font>
+#include <config_pc.h>
 #include "Demo.h"
-#include "text/lgfont.font"
 
 int Demo::run()
 {
@@ -15,20 +15,28 @@ int Demo::run()
         draw->clear();
         draw->setFgColor( 0x00, 0x00, 0x00 );
 
+        draw->offset( 10, 10 );
         snprintf( strbuff, sizeof( strbuff ), "number:%i", rand() % 100000 );
-        draw->string( strbuff, 5, 10, &lg_font );
+        draw->string( strbuff, 0, 0, &lg_font );
 
         snprintf( strbuff, sizeof( strbuff ), "letter:%c", rand() % 26 + 65 );
-        draw->string( strbuff, 5, 20, &lg_font );
+        draw->string( strbuff, 0, 10, &lg_font );
 
-        draw->string( "cancel", 5, 30, &lg_font );
-        draw->string( "help", 5, 40, &lg_font );
-        draw->string( "1234567890", 5, 50, &lg_font );
+        draw->string( "help", 0, 20, &lg_font );
+        draw->string( "cancel", 0, 30, &lg_font );
+        draw->string( "1234567890", 0, 40, &lg_font );
+        draw->unoffset();
 
-        draw->setFgColor( 0x88, 0x00, 0x00 );
-        draw->string( "Quit", 5, 115, &lg_font );
-        draw->setFgColor( 0x00, 0x00, 0x88 );
-        draw->string( "Okay", 97, 115, &lg_font );
+        draw->offset( 0, 111 );
+        draw->setFgColor( 0x66, 0x66, 0x66 );
+        draw->fill( 0, 0, WIDTH, 20 );
+        draw->setFgColor( 0x88, 0x88, 0x88 );
+        draw->fill( 90, 0, WIDTH, 20 );
+        draw->setFgColor( 0xff, 0xff, 0xff );
+        draw->string( "menu", 97, 5, &lg_font );
+        draw->setFgColor( 0x33, 0x33, 0x33 );
+        draw->box( -1, 0, WIDTH + 2, 30 );
+        draw->unoffset();
 
         draw->render();
 
